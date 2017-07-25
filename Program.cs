a5fadca32a57555267cb76ecb11bb453179771e2
Amandeep Singh <10012280@BCS.NET.NZ>
2017-07-25 13:29:11 +1200

    HELLO WORLD

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pgwk3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
